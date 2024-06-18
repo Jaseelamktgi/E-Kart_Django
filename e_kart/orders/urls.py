902e7a17fda373f0109c25f6068479ef415bdd97
jaseelamktgii@gmail <jaseelamktgi@gmail.com>
2024-06-18 15:12:06 +0530

    checkout and show all orders

--- e_kart/orders/urls.py
+++ e_kart/orders/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('cart', views.show_cart, name= 'cart'),
     path('add_to_cart', views.add_to_cart, name= 'add_to_cart'),
     path('remove_item_from_cart/<pk>', views.remove_item_from_cart, name= 'remove_item_from_cart'),
+    path('checkout_cart', views.checkout_cart, name= 'checkout_cart'),
+    path('show_orders', views.show_orders, name= 'show_orders'),
 ]
--- e_kart/orders/views.py
+++ e_kart/orders/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from . models import Order, OrderedItem, Product
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
-@login_required
+@login_required(login_url='account')
 def show_cart(request):
     user = request.user
     print(user)
@@ -17,7 +18,7 @@ def show_cart(request):
     }
     return render(request, 'cart.html', context)
 
-@login_required
+@login_required(login_url='account')
 def add_to_cart(request):
     if request.POST:
         print(request.POST)
@@ -58,9 +59,52 @@ def add_to_cart(request):
         return redirect('cart')
     return render(request, 'cart.html')
 
-@login_required
+@login_required(login_url='account')
 def remove_item_from_cart(request,pk):
     ordered_item = OrderedItem.objects.get(id = pk)
     if ordered_item :
         ordered_item.delete()
     return render(request, 'cart.html')
+
+@login_required(login_url='account')
+def checkout_cart(request):
+    if request.POST:
+        try:
+            # identify current user
+            user = request.user
+            customer = user.customer_profile
+            total = float(request.POST.get('total'))
+            size = request.POST.get('size')
+
+            # Find or create an order in the cart stage for the current user
+            order = Order.objects.get(
+                owner=customer, 
+                order_status=Order.CART_STAGE,
+            )
+            # Update order status
+            if order :
+                order.order_status = Order.ORDER_CONFIRMED
+                order.total_price = total
+                order.save()
+                status_message = 'Order Confirmed. Your item will be delivered within 2 days.'
+                messages.success(request, status_message)
+            else:
+                status_message = 'Order Not Confirmed. Please try again.' 
+                messages.error(request, status_message)
+
+        except Exception as e:
+            status_message = 'Something went wrong. Please try again.'
+            messages.error(request, status_message)
+
+    return redirect('cart')
+
+@login_required(login_url='account')
+def show_orders(request):
+    user = request.user
+    customer = user.customer_profile
+    all_orders = Order.objects.filter(owner = customer).exclude(order_status = Order.CART_STAGE)
+    context = {
+        'orders':all_orders
+    }
+    print(context)
+    return render(request, 'orders.html', context)
